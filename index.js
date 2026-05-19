ebbf10e99632143259eb940889a91259469c57a1
Mohd-Rafik <rafikmohdrafik60@gmail.com>
2026-05-19 11:11:38 +0530

    auth

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const authroutes = require('./src/routes/authroutes');
 app.use(express.json());
 app.use('/api/v1', authroutes);
 
+app.get('/', async (req, res) => {
+    return res.status(200).json({
+        message: "Welcome To Auth Backend"
+    })
+})
 
 app.listen(PORT, async (req, res) => {
     console.log(`Server is Running on PORT ${PORT}`);
